6bfb6e6655001a19422910eb1f36012194881acb
Kuseh <kusehsw@gmail.com>
2025-01-22 22:48:57 +0000

    modified the mmodule to include resources

--- modules/static-S3-Website-bucket/main.tf
+++ modules/static-S3-Website-bucket/main.tf
@@ -2,6 +2,7 @@
 
 resource "aws_s3_bucket" "static-website-bucket" {
     bucket = var.s3-bucket-name
+    force_destroy = true
     tags = var.tags
   
 }
@@ -20,10 +21,33 @@ resource "aws_s3_bucket_website_configuration" "s3-website-config" {
 
 
 
+resource "aws_s3_bucket_ownership_controls" "bucket-owner" {
+  bucket = aws_s3_bucket.static-website-bucket.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+  depends_on = [aws_s3_bucket_public_access_block.public-acl-block]
+  
+}
+
+
+resource "aws_s3_bucket_public_access_block" "public-acl-block" {
+  bucket = aws_s3_bucket.static-website-bucket.id  
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+
+  
+  
+}
+
 resource "aws_s3_bucket_acl" "s3-website-bucket-acl" {
     bucket = aws_s3_bucket.static-website-bucket.id
     acl = "public-read"
   
+  depends_on = [ aws_s3_bucket_ownership_controls.bucket-owner ]
 }
 
 
@@ -38,7 +62,7 @@ resource "aws_s3_bucket_policy" "s3-website-bucket-policy" {
         Sid       = "PublicReadGetObject"
         Effect    = "Allow"
         Principal = "*"
-        Action    = "s3:GetObject"
+        Action    = "*"
         Resource = [
           aws_s3_bucket.static-website-bucket.arn,
           "${aws_s3_bucket.static-website-bucket.arn}/*",
@@ -47,4 +71,6 @@ resource "aws_s3_bucket_policy" "s3-website-bucket-policy" {
     ]
   })
   
+
+  depends_on = [ aws_s3_bucket_public_access_block.public-acl-block ]
 }
